fd6884abf2fff7df29998027940c972eda7b5cf2
wallerjake <wallerjake@gmail.com>
2013-11-07 15:08:33 -0600

    Loop through all fields and add output to console

--- capycap.js
+++ capycap.js
@@ -1,37 +1,41 @@
 
 function copyToClipboard (text) {
-  window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);
+  console.log(text);
+  alert('Check your console for all the output');
 }
+
 
 function initContentScript() {
 
   document.addEventListener('mousedown', function(evt) {
     // alert("botton down");
-    if (event.altKey){
-        var el = event.target;
-        if (event.shiftKey){
-            if (el.nodeName == "INPUT"){
-                if (el.type == 'text'){
-                    //console.log("fill_in '"+el.id+"', :with => '"+el.value+"'");
-                    copyToClipboard("fill_in '"+el.id+"', :with => '"+el.value+"'");
-                };
-                if (el.type == 'radio'){
-                    //console.log("choose '"+el.id+"'");
-                    copyToClipboard("choose '"+el.id+"'");
-                };
-                if (el.type == 'checkbox'){
-                    if (document.getElementById(el.id).checked){
-                        // console.log("check '"+el.id+"'");
-                        copyToClipboard("check '"+el.id+"'");
-                    }else{
-                        // console.log("uncheck '"+el.id+"'");
-                        copyToClipboard("uncheck '"+el.id+"'");
-                    }
-                };
-            };
-        }else{
-            copyToClipboard(el.id)
-        };
+    if (event.altKey) {
+        var form_id = $('form').attr('id');
+
+        var copyToClipboardText = "";
+        $('#' + form_id + " *").filter(':input').each(function(){
+
+            // do not worry about diabled fields skip them
+            if (!$('#' + this.id).is(':disabled')) {
+              if (this.type == 'text' && this.value != '') {
+                  copyToClipboardText += "fill_in '" + this.id + "', :with => '" + this.value + "'" + "\n";
+              };
+
+              if (this.type == 'radio') {
+                  copyToClipboardText += "choose '" + this.id + "'" + "\n";
+              };
+
+              if (this.type == 'checkbox') {
+                  if ($('#' + this.id).is(':checked')) {
+                      copyToClipboardText += "check '" + this.id + "'" + "\n";
+                  } else {
+                      copyToClipboardText += "uncheck '" + this.id + "'" + "\n";
+                  }
+              };
+            }
+        });
+
+        copyToClipboard(copyToClipboardText);
     };  
   }, false);
 }
