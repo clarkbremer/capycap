5ed61a2b29fb308863010516ffb98c25792a677b
Clark Bremer <clarkbremer@gmail.com>
2013-11-09 12:19:53 -0600

    Remove debug output.

--- capycap.js
+++ capycap.js
@@ -78,9 +78,6 @@ function initContentScript() {
   
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
       if (request.cmd == "scrape"){
         copyAllToClipboard();
       }
