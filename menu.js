b6654f04b5a5d76e68310f4e7705cd2af923b612
Clark Bremer <clarkbremer@gmail.com>
2013-11-09 12:42:43 -0600

    Strip out dead code
    Strip out dead code from the example I cribbed from originally.

--- menu.js
+++ menu.js
@@ -1,14 +1,5 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// The onClicked callback function.
 function onClickHandler(info, tab) {
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //   chrome.tabs.sendMessage(tabs[0].id, {cmd: "scrape"});
-  //   alert("Sent message to tab ID: "+tabs[0].id);
-
-  // });
   chrome.tabs.sendMessage(tab.id, {cmd: "scrape"});
  
   console.log("item " + info.menuItemId + " was clicked");
@@ -20,11 +11,6 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function() {
-  // Create one test item for each context type.
-  var contexts = ["page","selection","link","editable","image","video",
-                  "audio"];
- 
-  // Create a menu item
   chrome.contextMenus.create({"title": "Generate Capybera Code", "id": "parent"});
- 
 });
+ 
