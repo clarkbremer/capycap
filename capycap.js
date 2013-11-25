94bc758640841aa277f5a891c64191b49f3cf850
wallerjake <wallerjake@gmail.com>
2013-11-25 08:55:30 -0600

    Loop through multiple forms if they exist and get fields for all

--- capycap.js
+++ capycap.js
@@ -5,34 +5,37 @@ function copyToClipboard (text) {
 
 
 function copyAllToClipboard() {
-  var form_id = $('form').attr('id');
-
   var copyToClipboardText = "";
-  $('#' + form_id + " *").filter(':input').each(function(){
-      // do not worry about diabled fields skip them
-      if (!$('#' + this.id).is(':disabled')) {
-        if (this.type == 'text' && this.value != '') {
-            copyToClipboardText += "fill_in '" + this.id + "', :with => '" + this.value + "'" + "\n";
-        };
 
-        if (this.type == 'radio' && $('#' + this.id).is(':checked')) {
-            copyToClipboardText += "choose '" + this.id + "'" + "\n";
-        };
+  $('form').each(function() {
+    var form_id = $(this).attr('id');
 
-        if (this.type == 'checkbox') {
-            if ($('#' + this.id).is(':checked')) {
-                copyToClipboardText += "check '" + this.id + "'" + "\n";
-            } else {
-                copyToClipboardText += "uncheck '" + this.id + "'" + "\n";
+    $('#' + form_id + " *").filter(':input').each(function(){
+        // do not worry about diabled fields skip them
+        if (!$('#' + this.id).is(':disabled')) {
+          if (this.type == 'text' && this.value != '') {
+              copyToClipboardText += "fill_in '" + this.id + "', :with => '" + this.value + "'" + "\n";
+          };
+
+          if (this.type == 'radio' && $('#' + this.id).is(':checked')) {
+              copyToClipboardText += "choose '" + this.id + "'" + "\n";
+          };
+
+          if (this.type == 'checkbox') {
+              if ($('#' + this.id).is(':checked')) {
+                  copyToClipboardText += "check '" + this.id + "'" + "\n";
+              } else {
+                  copyToClipboardText += "uncheck '" + this.id + "'" + "\n";
+              }
+          };
+
+          if (this.type == 'select-one') {
+            if ($('#' + this.id).val() != '') {
+              copyToClipboardText += "select('" + $('#' + this.id + ' option:selected').text() + "', :from => '" + this.id + "')" + "\n";
             }
-        };
-
-        if (this.type == 'select-one') {
-          if ($('#' + this.id).val() != '') {
-            copyToClipboardText += "select('" + $('#' + this.id + ' option:selected').text() + "', :from => '" + this.id + "')" + "\n";
           }
         }
-      }
+    });
   });
 
   console.log(copyToClipboardText);
